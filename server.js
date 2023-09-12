4d11e623656a5a98d6edf0f536788638899e1671
ban tit <bantit005@gmail.com>
2023-09-12 10:43:03 +0630

    added server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req,res)=>{
+	if(req.url==='/'){
+		fs.readFile('./index.html',(err,data)=>{
+		res.writeHead(200,{"Content-Type":'text/html'})
+			res.writeHead(200,{"Content-Type":'text/html'})
+			res.write(data)
+		})
+	} else if(req.url==='/style.css') {
+		fs.readFile('./style.css',(err,data)=>{
+			res.writeHead(200,{"Content-Type":'text/css'})
+				res.writeHead(200,{"Content-Type":'text/html'})
+				res.write(data)
+			})
+	}
+
+	res.end()  
+})
