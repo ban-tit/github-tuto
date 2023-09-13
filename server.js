da60fb44b8604c6b39f1f988cdc511347ee22759
ban tit <bantit005@gmail.com>
2023-09-13 09:48:58 +0630

    modified server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const http = require('http')
-
 const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
 	if(req.url==='/'){
 		fs.readFile('./index.html',(err,data)=>{
-		res.writeHead(200,{"Content-Type":'text/html'})
 			res.writeHead(200,{"Content-Type":'text/html'})
 			res.write(data)
 			res.end()
